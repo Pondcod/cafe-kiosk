88fd31f83e6282b76fb1ac1451616f9145101e52
Pattaradanai <pattaradanai@example.com>
2025-05-07 20:51:48 +0700

    d

--- front/src/component/WelcomeScreen.jsx
+++ front/src/component/WelcomeScreen.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const WelcomeScreen = () => {
   return (
-    <div className='bg-amber-200 py-0.5'>WelcomeScreen</div>
+    <div className='bg-amber-200 py-0.5'>Welcombill</div>
   )
 }
 
